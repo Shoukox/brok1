df19c671ad5dab0045e8138726bbc7b22e1dcbf2
Shoukox <58671746+Shoukox@users.noreply.github.com>
2022-06-16 20:42:27 +0500

    Update Variables.cs

--- Variables.cs
+++ Variables.cs
@@ -55,8 +55,8 @@ namespace brok1
         public static string[] adminFuncs = new[] { "editbalance", "editrandom", "editmoon" };
         public static string[] buttons = new[] { "Баланс", "Рулетка", "Тех. поддержка", "Информация", "Пополнить", "Вывести", "Назад", "Крутить", "Магазин", "1 крутка за 50р", "2 крутки за 100р", "5 круток за 250р" };
 
-        public static string qiwiToken = "3599523b7912c1a6cca174ead91fcaf1";
-        public static string publicQiwiToken = "48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iP5HoWvxahg8wy7DRg23M2hFL7fyV6DH4V6hawCPTS6T7BY7GHdHyBSG7jVrtdwrigUxDhUNQ4z4MyFiBvg1VUy5ujNuM1tn6uHTpjvK3LV";
-        public static string privateQiwiToken = "eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjYxbnlmaS0wMCIsInVzZXJfaWQiOiI3OTE0Mjk2MDE5MCIsInNlY3JldCI6ImM4N2RlY2Q2NmZmZTAxODIwNjIwMTA2YzFiMTg4MzFkMjI2MTZhNDUyNGQzMjA5NjgwYmNkNDZiNzIzMjg3YzMifX0=";
+        public static string qiwiToken = "";
+        public static string publicQiwiToken = "";
+        public static string privateQiwiToken = "=";
     }
 }
